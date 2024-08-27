a5e479335ec9687c069167e4d7509b10a8d82ab4
CryptoPrism-io <yogesh.sahu@cryptoprism.io>
2024-08-27 15:39:36 +0530

    Update dmv.py

--- dmv.py
+++ dmv.py
@@ -1736,7 +1736,5 @@ elapsed_time_minutes = elapsed_time_seconds / 60
 
 print(f"Cell execution time: {elapsed_time_minutes:.2f} minutes")
 
-# prompt: can we generate a requirement.txt for this
 
-!pip freeze > requirements.txt
 
